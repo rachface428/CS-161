f91b2a2cbfbe053f2aeb8252f4ceb8abaa059fdc
rachface428 <51375801+rachface428@users.noreply.github.com>
2022-06-22 21:55:56 -0700

    Delete RachelButler_6.22.2022_animal.py

--- RachelButler_6.22.2022_animal.py
+++
@@ -1,7 +0,0 @@
-# Author: Rachel Butler
-# GitHub username: rachface428
-# Date: 6/22/2022
-# Description: Asks username their favorite animal and then prints out that that is there favorite animal
-
-fave_animal = input("koala: ")
-print("Your favorite animal is the " + fave_animal + ".")
